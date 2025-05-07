7611f892e677aba27b69767a4f458429f261d296
rick-bren <kencodebren@gmail.com>
2025-05-07 10:51:57 +0530

    first

--- week1/Day1-Fundamentals/Level 1 Practice/Pen.java
+++
@@ -1,9 +0,0 @@
-public class Pen{
-	public static void main(String[] args){
-		int pen = 14;
-		int students = 3;
-		int PenPerStudent = pen/students;
-		int RemainingPen = pen%students;
-		System.out.print("The Pen Per Student is "+ PenPerStudent + " and the remaining pen not distributed is " + RemainingPen);
-	}
-}
